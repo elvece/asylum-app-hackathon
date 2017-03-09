56d70635c4b2f037bc310b573f61aa94989d308d
dizehacioglu <dh1272@nyu.edu>
2017-03-08 20:25:28 -0700

    form and some styling

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,12 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var countries = require('../../../data/countries');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Humanwire Refugee Application' });
 });
 
 router.get('/personal-info', function(req, res, next) {
-  res.render('personal-info', { title: 'Express' });
+  res.render('personal-info', { title: 'Humanwire Refugee Application', countries: countries });
+});
+
+router.get('/background', function(req, res, next) {
+  res.render('background', { title: 'Humanwire Refugee Application' });
+});
+
+router.get('/financial', function(req, res, next) {
+  res.render('financial', { title: 'Humanwire Refugee Application' });
+});
+
+
+router.get('/private-info', function(req, res, next) {
+  res.render('private-info', { title: 'Humanwire Refugee Application' });
 });
 
 module.exports = router;
