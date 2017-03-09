17226ba39c32b32f1f8655f4c209c720ae402d64
dizehacioglu <dh1272@nyu.edu>
2017-03-08 19:32:14 -0700

    added route to personal info page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/personal-info', function(req, res, next) {
+  res.render('personal-info', { title: 'Express' });
+});
+
 module.exports = router;
